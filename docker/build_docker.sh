ca9783e1032e1aa01356058ef2907472fee2cb32
gogo40 <pericles.raskolnikoff@gmail.com>
2015-04-22 15:27:42 -0300

    cmake-gui is working

--- docker/build_docker.sh
+++ docker/build_docker.sh
@@ -4,4 +4,11 @@ echo "Creating docker image..."
 sudo docker build -t gogo40/lpm_sandbox .
 
 echo "Opening bash..."
-sudo docker run -t -i gogo40/lpm_sandbox /bin/bash
+IMG=gogo40/lpm_sandbox
+
+XSOCK=/tmp/.X11-unix
+XAUTH=/tmp/.docker.xauth
+xauth nlist :0 | sed -e 's/^..../ffff/' | xauth -f $XAUTH nmerge -
+
+sudo docker run -p 6600 -P -i -t -v $XSOCK:$XSOCK $IMG
+ 
--- docker/run_docker.sh
+++ docker/run_docker.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+echo "Opening bash..."
 IMG=gogo40/lpm_sandbox
-XSOCK=/tmp/.X11-unix/X0
-sudo docker run -v $XSOCK:$XSOCK $IMG
+
+XSOCK=/tmp/.X11-unix
+XAUTH=/tmp/.docker.xauth
+xauth nlist :0 | sed -e 's/^..../ffff/' | xauth -f $XAUTH nmerge -
+
+sudo docker run -p 6600 -P -i -t -v $XSOCK:$XSOCK $IMG
+
