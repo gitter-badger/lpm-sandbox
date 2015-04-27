8bbc30db98eb5ebb26f4891dba167c2fc98fe607
gogo40 <pericles.raskolnikoff@gmail.com>
2015-04-27 16:03:46 -0300

    improve scripts

--- base/clean_docker.sh
+++ base/clean_docker.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 echo "Stoping all containers and images..."
-sudo docker stop $(sudo docker ps -a -q)
+sudo docker stop --time=5 $(sudo docker ps -a -q)
 
 echo "Deleting all containers and images..."
 #how to delete all containers and images
 # Delete all containers
-sudo docker rm $(sudo docker ps -a -q)
+sudo docker rm   $(sudo docker ps -a -q)
 # Delete all images
-sudo docker rmi $(sudo docker images -q)
+sudo docker rmi  $(sudo docker images -q)
