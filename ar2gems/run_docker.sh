959ecfc159eb7049ff19912faad0ec4d17b27ad0
gogo40 <pericles.raskolnikoff@gmail.com>
2015-04-27 13:12:17 -0300

    dockerfile is prepared to build VTK

--- ar2gems/run_docker.sh
+++ ar2gems/run_docker.sh
@@ -7,5 +7,5 @@ XSOCK=/tmp/.X11-unix
 XAUTH=/tmp/.docker.xauth
 xauth nlist $DISPLAY | sed -e 's/^..../ffff/' | xauth -f $XAUTH nmerge -
 
-sudo docker run -p 6600 -P -i -t -v $XSOCK:$XSOCK $IMG $1
+sudo docker run --rm -p 6600 -P -i -t -v $XSOCK:$XSOCK $IMG $1
 
