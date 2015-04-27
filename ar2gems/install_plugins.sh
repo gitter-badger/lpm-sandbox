874ddf7fdabc8dfadfc239c9d77e21bb32e185f5
gogo40 <pericles.raskolnikoff@gmail.com>
2015-04-27 13:15:28 -0300

    scripts added

---
+++ ar2gems/install_plugins.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+cp -rf ../ar2gems/plugins/* Release/bin/plugins/
+cp Release/lib/libar2gems_widgets.so Release/bin/plugins/designer/
+cp -rf ../lpm-ufrgs-sgems-plugins/*/*.ui Release/bin/plugins/Geostat/
+cp -rf ../lpm-ufrgs-sgems-plugins-build/*/*.so Release/bin/plugins/Geostat/
+
+
+sudo cp Release/lib/libar2gems_widgets.so /opt/Qt/Tools/QtCreator/bin/plugins/designer
+
