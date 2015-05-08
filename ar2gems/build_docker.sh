ccd2636dc199b38a269674e08a09e32249f8c26e
gogo40 <pericles.raskolnikoff@gmail.com>
2015-05-08 00:03:01 -0300

    remote sudo in docker scripts

--- ar2gems/build_docker.sh
+++ ar2gems/build_docker.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 echo "Creating docker image..."
-sudo docker build -t gogo40/lpm-sandbox:latest .
+docker build -t gogo40/lpm-sandbox:latest .
--- ar2gems/run_docker.sh
+++ ar2gems/run_docker.sh
@@ -7,5 +7,5 @@ XSOCK=/tmp/.X11-unix
 XAUTH=/tmp/.docker.xauth
 xauth nlist $DISPLAY | sed -e 's/^..../ffff/' | xauth -f $XAUTH nmerge -
 
-sudo docker run --rm -p 6600 -P -i -t -v $XSOCK:$XSOCK $IMG $1
+docker run --rm -p 6600 -P -i -t -v $XSOCK:$XSOCK $IMG $1
 
