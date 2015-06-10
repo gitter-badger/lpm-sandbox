cc5b8c4ee132a4200921dbf5bb36fd1eeb650e88
gogo40 <pericles.raskolnikoff@gmail.com>
2015-06-10 14:29:00 -0300

    ar2gems_com added

--- ar2gems/run_docker.sh
+++ ar2gems/run_docker.sh
@@ -8,4 +8,5 @@ XAUTH=/tmp/.docker.xauth
 xauth nlist $DISPLAY | sed -e 's/^..../ffff/' | xauth -f $XAUTH nmerge -
 
 docker run --rm -p 6600 -P -i -t -v $XSOCK:$XSOCK $IMG $1
+#docker run -v $PWD/output:/output --rm -p 6600 -P -i -t -v $XSOCK:$XSOCK $IMG $1
 
